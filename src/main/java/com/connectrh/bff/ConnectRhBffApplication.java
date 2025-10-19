065d04f0386864d70bf05b0d9777a8a495140b75
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2025-10-18 23:41:45 -0300

    base project

---
+++ src/main/java/com/connectrh/bff/ConnectRhBffApplication.java
@@ -0,0 +1,13 @@
+package com.connectrh.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectRhBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectRhBffApplication.class, args);
+	}
+
+}
